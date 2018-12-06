7979aa50e29f8d3332ea4bf129cacfb2b8444b8b
jloriente <jloriente@gmail.com>
2018-12-06 10:28:06 +0100

    Adding js to check for iOS

--- ios-pwa-banner.js
+++ ios-pwa-banner.js
@@ -2,3 +2,24 @@
 // JS to show a PWA banner in iOS similar to the default banner showed in Android
 console.log('loaded ios-pwa-banner.js');
 
+let disableBannerCheck = true;
+
+// Detects if device is on iOS 
+const isIos = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    console.log('UA')
+    console.log(userAgent);
+    return /iphone|ipad|ipod/.test( userAgent );
+}
+
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+document.addEventListener("DOMContentLoaded", function() {
+    if ( disableBannerCheck || (isIos() && !isInStandaloneMode()) ) {
+        console.log('Show banner');
+        document.getElementsByClassName('ios-pwa-banner')[0].classList.remove('hide-element');;
+        //this.setState({ showInstallMessage: true });
+    }
+});
+
